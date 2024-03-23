26019fd0eb853acef0ecac9f6f13c5159af0049b
Dawson Rubino <155998903+drubino8@users.noreply.github.com>
2024-03-23 01:20:08 +0000

    Update index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Check JS is Connected
-console.log('JS Connected');
+// Data
+const pics = ['https://sfccdesign.com/2023gradshow/wp-content/uploads/2023/05/dawson-r.png', 'https://sfccdesign.com/2023gradshow/wp-content/uploads/2023/05/dawson-r-action.png'];
+
+// Random Number
+const randNum = Math.floor(Math.random() * 2);
+
+// Get Stuff
+const randoImage = document.querySelector('img');
+
+// Set Stuff
+randoImage.src = pics[randNum];
